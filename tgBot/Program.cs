5106f7d351c937c57f4242a2ee8ef6840f7c67e7
Justin Scroggins <justinscroggins@outlook.com>
2020-11-21 12:28:46 -0500

    Changes

---
+++ tgBot/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace Awesome
+{
+    class Program
+    {
+        static ITelegramBotClient botClient;
+
+        static void Main()
+        {
+            botClient = new TelegramBotClient("1433082577:AAFtoVW7Tq4aqZj4oN7K1uMt5i_rC4kXRIw");
+
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine(
+              $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+            );
+
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+            botClient.StopReceiving();
+        }
+
+        static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Text != null)
+            {
+                Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+
+                await botClient.SendTextMessageAsync(
+                  chatId: e.Message.Chat,
+                  text: "You said:\n" + e.Message.Text
+                );
+            }
+        }
+    }
+}
