22a8afd15e42f78cd6aed18787691ec8519e281b
Justin Scroggins <justinscroggins@outlook.com>
2020-11-26 09:18:35 -0500

    Added scraper loop

--- telegramBot/TGBot.cs
+++ telegramBot/TGBot.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using tdwscraper;
 using Telegram.Bot;
+using Telegram.Bot.Args;
 
 namespace telegramBot
 {
     public class TGBot
     {
-        static TelegramBotClient Bot = new TelegramBotClient("");
+        static TelegramBotClient Bot = new TelegramBotClient("1433082577:AAFtoVW7Tq4aqZj4oN7K1uMt5i_rC4kXRIw");
 
         static void Main(string[] args)
         {
@@ -19,28 +19,64 @@ namespace telegramBot
         {
             if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
             {
-                Scrape scraper = new Scrape();
+                TGBot bot = new TGBot();
                 if (e.Message.Text.Equals("!sticky"))
                 {
-                    scraper.GetPostType(1);
-                }
-
-                if (e.Message.Text.Equals("!top"))
-                {
-                    scraper.GetPostType(2);
-                }
-
-                if (e.Message.Text.Equals("!rising"))
-                {
-                    scraper.GetPostType(3);
+                    bot.GetPostType(1);
                 }
 
                 if (e.Message.Text.Equals("!new"))
                 {
-                    scraper.GetPostType(4);
+                    bot.GetPostType(2);
+                }
+
+                if (e.Message.Text.Equals("!rising"))
+                {
+                    bot.GetPostType(3);
+                }
+
+                if (e.Message.Text.Equals("!top"))
+                {
+                    bot.GetPostType(4);
                 }
 
             }
+        }
+
+        public void GetPostType(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    GetPosts("https://thedonald.win/");
+                    break;
+                case 2:
+                    GetPosts("https://thedonald.win/new");
+                    break;
+                case 3:
+                    GetPosts("https://thedonald.win/rising");
+                    break;
+                case 4:
+                    GetPosts("https://thedonald.win/top");
+                    break;
+            }
+        }
+
+        public void GetPosts(string url)
+        {
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load(url);
+            bool isRunning = true;
+            while (isRunning)
+            {
+                foreach (var item in doc.DocumentNode.SelectNodes("//*[contains(@class, 'post')]"))
+                {
+                    Console.Write("------------------------------------------------");
+                    Console.WriteLine(item.InnerText);
+                    Console.Write("------------------------------------------------");
+                }
+            }
+
         }
 
     }
